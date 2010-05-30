a94a048cd7af70a9488e59a9ebe344afe8ab1f60
Eric Leclerc <eric.leclerc@gmail.com>
2010-05-30 15:56:23 -0400

    only display queries (don't show fetch, and other useless stuff)

--- Danceric/Controller/Plugin/Debug/Plugin/Doctrine.php
+++ Danceric/Controller/Plugin/Debug/Plugin/Doctrine.php
@@ -5,15 +5,29 @@
  * Enable it at the configuration step of ZFDebug 
  * (http://code.google.com/p/zfdebug/wiki/Installation)
  * Example:
- *     $options = array(
- *         'plugins' => array('Variables',
- *             'Danceric_Controller_Plugin_Debug_Plugin_Doctrine',
- *             'File',
- *             'Memory',
- *             'Time',
- *             'Exception'),
- *     );
- *     $debug = new ZFDebug_Controller_Plugin_Debug($options);
+ *     protected function _initZFDebug()
+ *     {
+ *         $autoloader = Zend_Loader_Autoloader::getInstance();
+ *         $autoloader->registerNamespace('ZFDebug');
+ *         $autoloader->registerNamespace('Danceric');
+ *     
+ *         // Ensure doctrine db instance is loaded
+ *         $this->bootstrap('doctrine');
+ *     
+ *         $options = array(
+ *             'plugins' => array('Variables',
+ *                 'Danceric_Controller_Plugin_Debug_Plugin_Doctrine',
+ *                 'File',
+ *                 'Memory',
+ *                 'Time',
+ *                 'Exception'
+ *             ),
+ *         );
+ *     
+ *         $debug = new ZFDebug_Controller_Plugin_Debug($options);
+ *     
+ *         $this->bootstrap('frontController');
+ *     }
  * 
  * @category   Danceric
  * @package    Danceric_Controller
@@ -76,15 +90,20 @@ class Danceric_Controller_Plugin_Debug_Plugin_Doctrine extends ZFDebug_Controlle
      */
     public function getTab()
     {
-        if (!$this->_profilers)
+        if (!$this->_profilers) {
             return 'No Profiler';
+        }
 
         foreach ($this->_profilers as $profiler) {
+            $queries = 0;
             $time = 0;
             foreach ($profiler as $event) {
-                $time += $event->getElapsedSecs();
+                if (in_array($event->getCode(), $this->getQueryEventCodes())) {
+                    $time += $event->getElapsedSecs();
+                    $queries += 1;
+                }
             }
-            $profilerInfo[] = $profiler->count() . ' in ' . round($time*1000, 2)  . ' ms';
+            $profilerInfo[] = $queries . ' in ' . round($time*1000, 2)  . ' ms';
         }
         $html = implode(' / ', $profilerInfo);
 
@@ -98,33 +117,44 @@ class Danceric_Controller_Plugin_Debug_Plugin_Doctrine extends ZFDebug_Controlle
      */
     public function getPanel()
     {
-        if (!$this->_profilers)
+        if (!$this->_profilers) {
             return '';
+        }
 
         $html = '<h4>Database queries</h4>';
         
         foreach ($this->_profilers as $name => $profiler) {
                 $html .= '<h4>Connection: '.$name.'</h4><ol>';
                 foreach ($profiler as $event) {
-                    if (in_array($event->getName(), array('query', 'execute', 'exec'))) {
+                    if (in_array($event->getCode(), $this->getQueryEventCodes())) {
                         $info = htmlspecialchars($event->getQuery());
-                    } else {
-                        $info = '<em>' . htmlspecialchars($event->getName()) . '</em>';
-                    }
 
-                    $html .= '<li><strong>[' . round($event->getElapsedSecs()*1000, 2) . ' ms]</strong> ';
-                    $html .= $info;
+                        $html .= '<li><strong>[' . round($event->getElapsedSecs()*1000, 2) . ' ms]</strong> ';
+                        $html .= $info;
                 
-                    $params = $event->getParams();
-                    if(!empty($params)) {
-                        $html .= '<ul><em>bindings:</em> <li>'. implode('</li><li>', $params) . '</li></ul>';
+                        $params = $event->getParams();
+                        if(!empty($params)) {
+                            $html .= '<ul><em>bindings:</em> <li>'. implode('</li><li>', $params) . '</li></ul>';
+                        }
+                        $html .= '</li>';
                     }
-                    $html .= '</li>';
                 }
                 $html .= '</ol>';
         }
 
         return $html;
+    }
+    
+    /**
+     * return codes for 'query' type of event
+     */
+    protected function getQueryEventCodes()
+    {
+        return array(
+            Doctrine_Event::CONN_EXEC, 
+            Doctrine_Event::STMT_EXECUTE,
+            Doctrine_Event::CONN_QUERY,
+        );
     }
 
 }
